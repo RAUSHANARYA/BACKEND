2ce91420150459d3307ca0d1102c69d4e0260041
RAUSHANARYA <raushankumarlnct07@gmail.com>
2026-06-25 19:30:17 +0530

    user.module

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,32 +1,75 @@
-//used fo creating functions
-import User from "../models/user.model";
+// used for creating functions
 
-export const RegisterUser = async (req,res) => {
-   try{
-     const{fullName , email , phone ,password,gender,dob} = req.body;
+import User from "../models/user.model.js";
 
-     if(!fullName || !email || !gender || !dob || !phone || !password)
-     {
-        res.status(400).json({message:"All feilds Required"});
-        return;
-     }
-     const existingUser = await User.findOne({email});
-     if(existingUser)
-     {
-        res.status(409).json({message:"email Already used "});
-        return ;
-     }
-   }
-   catch(error)
-   {
+export const RegisterUser = async (req, res) => {
+    try {
+        const {
+            fullName,
+            email,
+            phone,
+            password,
+            gender,
+            dob,
+        } = req.body;
 
-   }
-}
+        if (!fullName || !email || !gender || !dob || !phone || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required",
+            });
+        }
 
-export const Logout_user = (req,res) => {
-    res.json({message : "Logout Successfully from controller"});
-}
+        const existingUser = await User.findOne({ email });
 
-export const Login_user = (req,res) => {
-    res.json({message : "Login Successfully from controller"});
-}
+        if (existingUser) {
+            return res.status(409).json({
+                success: false,
+                message: "Email already exists",
+            });
+        }
+
+       const photoUrl = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
+
+        const photo = {
+            url: photoURL,
+            publicID: null,
+        };
+
+        const newUser = await User.create({
+            fullName,
+            email,
+            phone,
+            password,
+            gender,
+            dob,
+            photo,
+        });
+
+         res.status(201).json({
+            success: true,
+            message: "User Registered Successfully",
+            user: newUser,
+        });
+
+    } catch (error) {
+        console.log(error);
+
+         res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+    }
+};
+
+export const Logout_user = (req, res) => {
+    res.json({
+        message: "Logout Successfully from controller",
+    });
+};
+
+export const Login_user = (req, res) => {
+    res.json({
+        message: "Login Successfully from controller",
+    });
+};
--- src/routers/auth.route.js
+++ src/routers/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { Login_user , Logout_user , Register_user } from "../controllers/auth.controller.js";
+import { Login_user, Logout_user, RegisterUser } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/login" , Login_user);
-router.post("/register" , Register_user);
+router.post("/register", RegisterUser);
 router.get("/logout" , Logout_user);
 
 export default router;
